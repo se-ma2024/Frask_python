43bbb3ccfef7bef2d5f8ac80b66edadea3675825
se-ma2024 <94701414+se-ma2024@users.noreply.github.com>
2024-01-12 18:46:50 +0900

    create memo

--- app.py
+++ app.py
@@ -17,11 +17,42 @@ def add():
     tasks.append(task)
     return redirect(url_for('todo_list'))
 
-@app.route('/delete/<task_index>')
+@app.route('/delete/<int:task_index>')
 def delete(task_index):
-    if task_index.isdigit() and 0 <= int(task_index) < len(tasks):
-        del tasks[int(task_index)]
+    if 0 <= task_index < len(tasks):
+        del tasks[task_index]
     return redirect(url_for('todo_list'))
+
+memos = []
+
+@app.route('/memo')
+def memo():
+    return render_template("memo.html", memos=memos)
+
+@app.route('/add_memo', methods=['POST'])
+def add_memo():
+    memo_text = request.form.get("memo")
+    memos.append(memo_text)
+    return redirect(url_for("memo"))
+
+@app.route('/edit_memo/<int:memo_index>')
+def edit_memo(memo_index):
+    if 0 <= memo_index < len(memos):
+        return render_template('edit_memo.html', memo_index=memo_index, memo=memos[memo_index])
+    else:
+        return redirect(url_for('memo'))
+
+@app.route('/update_memo/<int:memo_index>', methods=['POST'])
+def update_memo(memo_index):
+    if 0 <= memo_index < len(memos):
+        memos[memo_index] = request.form.get('memo')
+    return redirect(url_for('memo'))
+
+@app.route('/delete_memo/<int:memo_index>')
+def delete_memo(memo_index):
+    if 0 <= memo_index < len(memos):
+        del memos[memo_index]
+    return redirect(url_for('memo'))
 
 if __name__ == '__main__':
     app.run(debug=True)
